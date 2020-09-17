74e9e9ef7e1dee2ec151533c4c9448a89222d4a5
Lejing Huang <ohaluminum@users.noreply.github.com>
2020-09-17 11:34:17 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "ArgumentManager.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    ArgumentManager am(argc, argv);
+
+    //Get the filename of argument name "input" and "output"
+    //string input = am.get("input");
+    //string output = am.get("output");
+
+    //Test
+    string input = "input23.txt";
+    string output = "output23.txt";
+    
+
+
+
+
+
+
+    return 0;
+}
